d15483c40ecb9ab5d458358d34a6ac56cbe1587e
FengXiang2233 <fjx13125@163.com>
2024-01-30 15:09:19 +0800

    提交同步

---
+++ core/module/StartFrp.py
@@ -0,0 +1,9 @@
+from core.network import ChmlfrpAPI
+from core.initialize import CheckFile
+
+# 启动frp
+def start(tun_id):
+    if CheckFile.CheckFrp():
+        # 检查用户是否拥有该隧道
+        data=ChmlfrpAPI.user_tun()
+    else:return False
