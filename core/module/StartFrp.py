91cc76440136a023d26e93356d0f698df97501fe
FengXiang2233 <fjx13125@163.com>
2024-01-31 13:56:46 +0800

    标准化返回

--- core/module/StartFrp.py
+++ core/module/StartFrp.py
@@ -10,22 +10,26 @@ def start(tun_id):
     if CheckFile.CheckFrp():
         # 检查用户是否拥有该隧道
         data=ChmlfrpAPI.user_tun()
-        fash=False
-        for da in data:
-            if da["id"]==tun_id:
-                fash=True
-                break
-        if fash:
-            # 驱动frp核心
-            if os.name=="nt":
-                os.system("start frpc.exe"+" -u "+User.token+" -p "+tun_id)
+        if data[0]==True:
+            fash=False
+            for da in data:
+                if da["id"]==tun_id:
+                    fash=True
+                    break
+            if fash:
+                # 驱动frp核心
+                if os.name=="nt":
+                    os.system("start frpc.exe"+" -u "+User.token+" -p "+tun_id)
+                else:
+                    os.system("start frpc"+" -u "+User.token+" -p "+tun_id)
+                log.info(f"启动frp: token:{User.token} ID:{tun_id} 已拉起")
+                return False,"已拉起frp核心"
             else:
-                os.system("start frpc"+" -u "+User.token+" -p "+tun_id)
-            log.info(f"启动frp: token:{User.token} ID:{tun_id} 已拉起")
-            return "已拉起frp核心"
+                log.warn(f"启动frp: ID:{tun_id} 不属于token:{User.token}")
+                return False,f"ID:{tun_id} 不属于你"
         else:
-            log.warn(f"启动frp: ID:{tun_id} 不属于token:{User.token}")
-            return f"ID:{tun_id} 不属于你"
+            log.error("启动frp: 未知错误")
+            return False,"未知错误"
     else:
         log.error("启动frp: 未知错误")
-        return "未知错误"
+        return False,"未知错误"
--- core/network/ChmlfrpAPI.py
+++ core/network/ChmlfrpAPI.py
@@ -13,16 +13,16 @@ def login(Name,Password):
         User.id = data['userid']
         User.token = data['token']
         User.LoginData = data
-        return True
+        return True,None
     except:
         log.error("登录失败")
-        return False
+        return False,None
 
 # 隧道列表获取
 def user_tun():
     try:
         log.info(f"token:{User.token} 尝试获取隧道信息")
         data=requests.get("https://panel.chmlfrp.cn/api/usertunnel.php",{"token":User.token}).json()
-        return data
+        return True,data
     except:
-        return None
+        return False,None
