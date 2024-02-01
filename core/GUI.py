219ef54feefaf4d7bd7c17693d43eb310f4efde0
FengXiang2233 <fjx13125@163.com>
2024-02-01 17:43:54 +0800

    添加主界面背景/微调主界面

--- core/GUI.py
+++ core/GUI.py
@@ -1,7 +1,11 @@
 from core.network import ChmlfrpAPI
-
+from core.g_var import User
+from PIL import Image, ImageTk
+from io import BytesIO
+import requests as reqt
 import customtkinter as ctk
 import tkinter.messagebox
+
 
 class info_window:
     def info(title:str,text:str):
@@ -51,16 +55,22 @@ class App(ctk.CTk):
         self.geometry("730x420")
         self.iconbitmap("./chmlfrp.ico")
         self.resizable(0, 0)
+        # 处理/下载背景图片
+        if not User.LoginData['background_img']=="":
+            self.bg=Image.open(BytesIO(reqt.get(User.LoginData['background_img']).content))
+            self.bg=self.bg.resize((730,420))
+            self.bg=ImageTk.PhotoImage(self.bg)
+            self.bg_label=ctk.CTkLabel(self,text="",image=self.bg)
+            self.bg_label.place(relx=0,rely=0)
         # start frp
-        self.optionmenu = ctk.CTkOptionMenu(self,height=36,command=self.optionmenu_callback,values=["1","2","3","4"])
+        self.optionmenu = ctk.CTkOptionMenu(self,height=41,corner_radius=0,command=self.optionmenu_callback,values=["1","2","3","4"])
         self.optionmenu.place(relx=0.77,rely=0.85)
-        self.start_frp_button=ctk.CTkButton(self,text="Start Frp\nNone",height=35,command=self.button_callback)
+        self.start_frp_button=ctk.CTkButton(self,text="Start Frp\n#0 你TM倒是选a",corner_radius=0,state="disabled",height=40,command=self.button_callback)
         self.start_frp_button.place(relx=0.72,rely=0.85)
 
     # 更新start frp按钮的信息
     def optionmenu_callback(self,choice):
-        self.start_frp_button.configure(text=f"Start Frp\n{choice}")
-
+        self.start_frp_button.configure(text=f"Start Frp\n{choice}",state="normal")
     def button_callback(self):
         print("button pressed")
 
@@ -69,6 +79,5 @@ def run():
     login.mainloop()
 
 def main():
-    ctk.set_appearance_mode('Dark')
     app=App()
     app.mainloop()
